c946b5a10e9d55e3f316cc4a39e2a26b61cfefa3
Ardiantohendy <bayuardianto421@gmail.com>
2023-11-05 23:45:22 +0700

    new commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import "../Header/Header.css";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        .club. <span></span>
+      </h1>
+      <h2>Pool & Cafe</h2>
+      <ul>
+        <li>
+          <a href="#">reservation</a>
+        </li>
+        <li>
+          <a href="#">waiting list</a>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;

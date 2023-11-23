f625c005f266a0a5c90b07c92cbf99bbac85e8d3
Ardiantohendy <bayuardianto421@gmail.com>
2023-11-23 22:55:15 +0700

    new commit

--- src/datas/Data.jsx
+++ src/datas/Data.jsx
@@ -6,19 +6,19 @@ export const DataForMenu = {
   pool: {
     title: "Pool",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero itaque sit voluptates. Accusamus ex dolor neque veritatis odio iste perspiciatis necessitatibus veniam voluptatum omnis est sint alias, commodi praesentium, ipsa a! Provident illum quidem impedit eligendi voluptatibus, nulla libero tenetur asperiores repudiandae harum. Harum corporis assumenda, ipsum est nostrum quasi. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium deserunt doloremque enim corporis, error voluptas! Culpa enim ex tempore tenetur!",
+      "Join us for a laid-back and enjoyable time at our billiards lounge. Whether you're a seasoned pro or just looking for a friendly game, our vibrant atmosphere and well-maintained tables provide the perfect backdrop for a casual and fun billiards experience. Grab a cue, sink some shots, and let the good times roll!. Challenge your friends to a friendly game or simply relax with a cue in hand. Our billiards lounge offers a welcoming space for players of all skill levels. With the satisfying crack of the balls and the camaraderie in the air, our venue is the ideal spot to unwind, socialize, and enjoy the timeless thrill of a great game of billiards.",
     image: poolImg,
   },
   cafe: {
     title: "Bar & Cafe",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero itaque sit voluptates. Accusamus ex dolor neque veritatis odio iste perspiciatis necessitatibus veniam voluptatum omnis est sint alias, commodi praesentium, ipsa a! Provident illum quidem impedit eligendi voluptatibus, nulla libero tenetur asperiores repudiandae harum. Harum corporis assumenda, ipsum est nostrum quasi. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium deserunt doloremque enim corporis, error voluptas! Culpa enim ex tempore tenetur!",
+      "Step into our cafe nestled within the heart of our billiards lounge. Indulge your senses in the aromatic embrace of freshly brewed coffee and the delightful hum of friendly competition. Whether you're taking a break between games or simply savoring a moment of relaxation, our cafe offers a tempting selection of beverages and snacks. Enjoy the perfect blend of skillful play and culinary delights, where each sip and bite enhances the overall experience. Our cafe is designed to be the cozy hub where great games and great flavors seamlessly come together.",
     image: cafeImg,
   },
   live_music: {
     title: "Live Music",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero itaque sit voluptates. Accusamus ex dolor neque veritatis odio iste perspiciatis necessitatibus veniam voluptatum omnis est sint alias, commodi praesentium, ipsa a! Provident illum quidem impedit eligendi voluptatibus, nulla libero tenetur asperiores repudiandae harum. Harum corporis assumenda, ipsum est nostrum quasi. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium deserunt doloremque enim corporis, error voluptas! Culpa enim ex tempore tenetur!",
+      "Feel the rhythm of the night come alive in our billiards lounge with live music that sets the perfect tone for an unforgettable experience. Immerse yourself in the soulful tunes as you enjoy a game of billiards or unwind in our cozy cafe. Our carefully curated live music events feature talented artists spanning various genres, adding a vibrant soundtrack to your evening. Whether you're a music enthusiast, a billiards aficionado, or someone seeking a lively atmosphere, our lounge is where the magic happens. Join us for a harmonious blend of skill, taste, and the joy of live music, creating memories that resonate long after the last note fades away.",
     image: musicImg,
   },
 };
